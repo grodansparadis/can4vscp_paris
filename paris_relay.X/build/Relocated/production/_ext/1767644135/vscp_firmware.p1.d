5139e34e6bebdb66124b2edad7f2f09a9f69398d
Ake Hedman, Paradise of the Frog <akhe@grodansparadis.com>
2015-02-12 23:07:57 +0100

    Added project folder

---
+++ paris_relay.X/build/Relocated/production/_ext/1767644135/vscp_firmware.p1.d
@@ -0,0 +1,9 @@
+ build/Relocated/production/_ext/1767644135/vscp_firmware.d  \
+ build/Relocated/production/_ext/1767644135/vscp_firmware.p1:  \
+ ../../../../../vscp_firmware/common/vscp_firmware.c  \
+../../../../../vscp_software/src/vscp/common/vscp_type.h  \
+../vscp_projdefs.h  \
+../vscp_compiler.h  \
+../../../../../vscp_software/src/vscp/common/vscp_class.h  \
+../../../../../vscp_firmware/common/vscp_firmware.h  \
+../../../../../vscp_firmware/pic/common/inttypes.h 
---
+++ paris_relay.X/build/Relocated/production/_ext/382426430/eeprom.p1.d
@@ -0,0 +1,6 @@
+ build/Relocated/production/_ext/382426430/eeprom.d  \
+ build/Relocated/production/_ext/382426430/eeprom.p1:  \
+ ../../../../../vscp_firmware/pic/common/eeprom.c  \
+../vscp_projdefs.h  \
+../vscp_compiler.h  \
+../../../../../vscp_firmware/pic/common/inttypes.h 
